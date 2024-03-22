a7d0170377f3e8aedba655c4f1ce1bc63c94ad4f
musasabi23 <oniokooniono0209@gmail.com>
2024-03-22 23:38:30 +0900

    1

--- Example/obj/Debug/net8.0/Example.AssemblyInfo.cs
+++ Example/obj/Debug/net8.0/Example.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4fa93250c40b7eb0f1326370bfe27c24da9d329")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
